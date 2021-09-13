2d76ea98e3223aac959ffe74b8564b1afe06e16b
NOOB4FUN <90191235+NOOB4FUN@users.noreply.github.com>
2021-09-13 13:16:49 +0200

    ✨ matte

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+
+
 print("hej på er EE20")
 name = input("vad heter du")
 print (f"hej på dig mango")
@@ -6,3 +8,21 @@ age = input("hur gammal är du")
 print(f"oj så gammal du är, hela 500 år.")
 
 print("nu går jag hem.")
+
+tal1 = 12
+tal2 = 12
+
+tal1 = 12023
+tal2 = float(input("skriv ett tal: "))
+
+summa = tal1 * tal2
+print(f"summan av {tal1} * {tal2} blir {summa}")
+
+summa = tal1 - tal2
+print(f"summan blir {summa}")
+
+summa = tal1 + tal2 
+print(f"summan blir {summa}")
+
+summa = tal1 / tal2
+print(f"summan blir {summa}")
